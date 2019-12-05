6cd9101bf564741b4b9c6dc9ea15eae4bfba0575
Lenovo <Lenovo@LAPTOP-M2KPVUIE>
2019-12-05 10:26:42 +0800

    sdfg

---
+++ src/main/java/cms_day05/Demo2.java
@@ -0,0 +1,13 @@
+package cms_day05;
+
+/**
+ * @author 张琳涓
+ * @Date:2019年12月5日
+ */
+public class Demo2 {
+	public static void main(String[] args) {
+		
+		System.out.println("helloWord");
+		
+	}
+}
